55503b6a82ed7d4eb307380b279708fd1dcf67fc
Zak B. Elep <zakame@zakame.net>
2016-10-09 00:05:44 +0800

    Don't bork if there are no cities
    Acked-by: Zak B. Elep <zakame@zakame.net>

--- backend/drunkery/lib/Drunkery/Search.pm
+++ backend/drunkery/lib/Drunkery/Search.pm
@@ -20,7 +20,8 @@ sub by_city {
     my $city_ish = $self->ua->get($url)->res->json;
 
     # limit to the first result for now
-    my @cities = ( grep { $_->{dest_type} eq 'city' } @$city_ish )[0];
+    my @cities = grep { $_->{dest_type} eq 'city' } @$city_ish;
+    @cities = ( $cities[0] ) if @cities > 1;
 
     # resolve hotels & breweries found in each city via BreweryDB
     my ( @breweries, @hotels );
